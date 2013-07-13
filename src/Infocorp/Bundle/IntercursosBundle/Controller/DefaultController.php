7b20e588fff62b4138f166c552919edd3cc30ea8
EduardoMRB <eduardomrb@gmail.com>
2013-07-13 00:36:46 -0400

    Adiciona filtro por categorias

--- src/Infocorp/Bundle/IntercursosBundle/Controller/DefaultController.php
+++ src/Infocorp/Bundle/IntercursosBundle/Controller/DefaultController.php
@@ -23,27 +23,49 @@ class DefaultController extends Controller
 
     public function newsAction()
     {
+        $categoriesManager = $this->get('sonata.news.manager.category');
         $news = $this
             ->getDoctrine()
             ->getRepository('ApplicationSonataNewsBundle:Post')
             ->findAll()
         ;
 
+        $categories = $categoriesManager->findBy(array('enabled' => true));
+        
         return $this->render('InfocorpIntercursosBundle:Default:news.html.twig', array(
             'news' => $news,
+            'categories' => $categories,
         ));
     }
 
-    public function showNewsAction($id)
+    public function showNewsAction($slug)
     {
         $news = $this
             ->getDoctrine()
             ->getRepository('ApplicationSonataNewsBundle:Post')
-            ->find($id)
+            ->findOneBy(array('slug' => $slug))
         ;
         
         return $this->render('InfocorpIntercursosBundle:Default:show_news.html.twig', array(
             'news' => $news,
+        ));
+    }
+
+    public function showCategoryAction($id)
+    {
+        $categoriesManager = $this->get('sonata.news.manager.category');
+
+        $categories = $categoriesManager->findBy(array('enabled' => true));
+
+        $news = $this
+            ->getDoctrine()
+            ->getRepository('ApplicationSonataNewsBundle:Post')
+            ->findBy(array('category' => $categoriesManager->findOneBy(array('id' => $id))))
+        ;
+
+        return $this->render('InfocorpIntercursosBundle:Default:news.html.twig', array(
+            'news' => $news,
+            'categories' => $categories,
         ));
     }
 }
