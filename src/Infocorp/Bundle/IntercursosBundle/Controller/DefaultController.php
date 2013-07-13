50a2b1ecbb0f65cdd513f060a225f687215cc22e
EduardoMRB <eduardomrb@gmail.com>
2013-07-12 23:03:09 -0400

    Adiciona rotas e views para noticias

--- src/Infocorp/Bundle/IntercursosBundle/Controller/DefaultController.php
+++ src/Infocorp/Bundle/IntercursosBundle/Controller/DefaultController.php
@@ -20,4 +20,30 @@ class DefaultController extends Controller
             'news' => $news,
         ));
     }
+
+    public function newsAction()
+    {
+        $news = $this
+            ->getDoctrine()
+            ->getRepository('ApplicationSonataNewsBundle:Post')
+            ->findAll()
+        ;
+
+        return $this->render('InfocorpIntercursosBundle:Default:news.html.twig', array(
+            'news' => $news,
+        ));
+    }
+
+    public function showNewsAction($id)
+    {
+        $news = $this
+            ->getDoctrine()
+            ->getRepository('ApplicationSonataNewsBundle:Post')
+            ->find($id)
+        ;
+        
+        return $this->render('InfocorpIntercursosBundle:Default:show_news.html.twig', array(
+            'news' => $news,
+        ));
+    }
 }
